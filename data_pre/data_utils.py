ab720293386919e21e5d7dae60d29666637a070d
NeoNexusU6 <NeoNexus>
2024-12-16 00:42:26 +0800

    regular update

--- data_pre/data_utils.py
+++ data_pre/data_utils.py
@@ -6,91 +6,57 @@ import requests
 from rdkit import Chem
 from rdkit.Chem import rdMolDescriptors
 
-def tran_smiles_iso2can_pub(canonical_smiles):
+BASE_URL = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound"
+
+# query_arg
+NAME_BASE_FINDER = "/name/property"
+SMILES_BASE_FINDER = "/smiles/property"
+
+ISOMERIC_SMILES_RESULT = "/IsomericSMILES"
+URL_TXT_END = "/TXT"
+SMILES_QUERY_ARG = "?smiles="
+
+
+def restful_pub_finder(query_arg, query_base=NAME_BASE_FINDER):
     """
     used for converting canonical_smiles to smiles through pubchem,speed : slowly
     If not find return None else return String
     
     canonical_smiles : String
-    """ 
-
-    if canonical_smiles:
-        # 将Smiles名称进行URL编码
-        canonical_smiles_url = urllib.parse.quote(canonical_smiles,safe='')
-        # 构建URL
-        # url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/{canonical_smiles_url}/property/IsomericSMILES/TXT"
-        url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/property/IsomericSMILES/TXT?smiles={canonical_smiles_url}"
-        if canonical_smiles_url:
-            while True:
-                try:
-                    # 发起GET请求
-                    response = requests.get(url)
-                    
-                    # 如果请求成功且返回200
-                    if response.status_code == 200:
-                        # 处理响应内容，提取SMILES编码
-                        smiles_pbc = response.text.strip()
-                        
-                        # 如果返回数据为空，表示没有对应数据
-                        if not smiles_pbc:
-                            print(f"No SMILES data found for {canonical_smiles}")
-                            return None
-                        
-                        # 返回SMILES编码
-                        return smiles_pbc
-                    
-                    # 如果返回的状态码不是200，打印错误并退出
-                    else:
-                        print(f"Error: {response.status_code} - Failed to retrieve data for {canonical_smiles}")
-                        return None
-                
-                except requests.RequestException as e:
-                    # 捕获网络请求异常，打印错误信息并等待2秒重试
-                    print(f"Network error: {e}. Retrying in 2 seconds...")
-                    time.sleep(1)
-        else:
-            return None
-
-def tran_name2iso_pub(name):
     """
-    used for converting canonical_smiles to smiles through pubchem,speed : slowly
-    If not find return None else return String
-    
-    canonical_smiles : String
-    """ 
 
-    if name:
+    if query_arg:
         # 将Smiles名称进行URL编码
-        name_url = urllib.parse.quote(name,safe='')
+        query_arg_url = urllib.parse.quote(query_arg, safe='')
         # 构建URL
-        url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{name_url}/property/IsomericSMILES/TXT"
-        if name_url:
+        url = BASE_URL + query_base + ISOMERIC_SMILES_RESULT + URL_TXT_END + SMILES_QUERY_ARG + f"{query_arg_url}"
+        if query_arg_url:
             while True:
                 try:
                     # 发起GET请求
                     response = requests.get(url)
-                    
+
                     # 如果请求成功且返回200
                     if response.status_code == 200:
                         # 处理响应内容，提取SMILES编码
                         smiles_pbc = response.text.strip()
-                        
+
                         # 如果返回数据为空，表示没有对应数据
                         if not smiles_pbc:
-                            print(f"No SMILES data found for {name}")
+                            print(f"No SMILES data found for {query_arg}")
                             return None
-                        
+
                         # 返回SMILES编码
                         if '\n' in smiles_pbc:
                             smiles_pbc = None
-                        print(f'return smiles {smiles_pbc}')
+                        print(f"{query_arg}\r\nsmiles_pbc find :{smiles_pbc}\r\n")
                         return smiles_pbc
-                    
+
                     # 如果返回的状态码不是200，打印错误并退出
                     else:
-                        print(f"Error: {response.status_code} - Failed to retrieve data for {name}")
+                        print(f"Error: {response.status_code} - Failed to retrieve data for {query_arg}")
                         return None
-                
+
                 except requests.RequestException as e:
                     # 捕获网络请求异常，打印错误信息并等待2秒重试
                     print(f"Network error: {e}. Retrying in 2 seconds...")
@@ -98,7 +64,8 @@ def tran_name2iso_pub(name):
         else:
             return None
 
-def tran_iupac2smiles_cir(compund):
+
+def tran_iupac2can_smiles_cir(compund):
     """
     Used for convert iupac_name/smiles(to fix smiles) to canonical_smiles
     If not find return None else return String
@@ -108,8 +75,10 @@ def tran_iupac2smiles_cir(compund):
         url = 'http://cactus.nci.nih.gov/chemical/structure/' + quote(compund) + '/smiles'
         ans = urlopen(url).read().decode('utf8')
         return ans
+
     except:
         return None
+
 
 def tran_iso2can_rdkit(smiles):
     """
@@ -118,20 +87,23 @@ def tran_iso2can_rdkit(smiles):
 
     smiles : String
     """
-    isomericsmiles = None
+
     try:
         isomericsmiles = Chem.MolToSmiles(Chem.MolFromSmiles(smiles), canonical=True, isomericSmiles=False)
+
     except Exception:
         # Catch any other unexpected errors
         print(f"Unexpected error: \r\n{smiles}")
         return None
-    
+
     if isomericsmiles:
         return isomericsmiles
 
     else:
-        #print(f'SMILES not find now ans is {smiles}\r\n{isomericsmiles}')
-        return None
+        print(f'SMILES not find now ans is {smiles}\r\n'
+              f'{isomericsmiles}')
+        return isomericsmiles
+
 
 def calculate_ecfp_rdkit(smiles, radius=2, n_bits=1024):
     """
@@ -146,9 +118,10 @@ def calculate_ecfp_rdkit(smiles, radius=2, n_bits=1024):
     mol = Chem.MolFromSmiles(smiles)
     if mol is not None:  # 如果SMILES字符串有效
         ecfp = rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, radius, nBits=n_bits)
-        return list(ecfp) # 转换为list数组
+        return list(ecfp)  # 转换为list数组
     else:
         return None  # 如果SMILES无效，则返回一个全0的指纹
+
 
 def tran_iupac2smiles_fun(compound):
     """
@@ -156,7 +129,10 @@ def tran_iupac2smiles_fun(compound):
     
     compound : String [smiles or iupacname of the compound]
     """
-    canonical_smiles = tran_iupac2smiles_cir(compound) if compound else None
+    canonical_smiles = tran_iupac2can_smiles_cir(compound) if compound else None
+
+    print(f'{compound}\r\n'
+          f'canonical_smiles: {canonical_smiles}\r\n')
 
     isomericsmiles = tran_iso2can_rdkit(canonical_smiles) if canonical_smiles else None
 
---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from data_pre import ALLCCS_PATH, TEMP_SAVE, MELTIN_PATH
+from data_pre.data_module import Data_reader_METLIN
+
+if __name__ == '__main__':
+
+    meltin_tester = Data_reader_METLIN(MELTIN_PATH,
+                                       fun=lambda path, col_name: pd.read_csv(path, usecols=col_name))
+
+    # select specific data
+    meltin_tester.selected_proprties({'Dimer.1': 'Monomer'})
+    meltin_tester.iso2can_smiles_cir()
+    meltin_tester.data.info()
+    meltin_tester.can2iso_smiles_pub()
+    # write data as csv
+    meltin_tester.data.to_csv(TEMP_SAVE + 'METLIN.csv', index=False)
